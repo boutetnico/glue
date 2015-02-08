4fb2c07c90a2480b9083f9026eb93fc82c11fcb1
Nicolas Boutet <amd3002@gmail.com>
2015-02-08 16:05:40 +0100

    Remove debug

--- glue/algorithms/square_ordered.py
+++ glue/algorithms/square_ordered.py
@@ -7,7 +7,6 @@ class SquareOrderedAlgorithm(object):
     def process(self, sprite):
         num_images = len(sprite.images)
         max_width = round(math.sqrt(num_images))
-        print "max_width = %d, num_images = %d" % (max_width, num_images)
         x = 0
         y = 0
         for image in sprite.images:
@@ -16,5 +15,4 @@ class SquareOrderedAlgorithm(object):
                 y += 1
             image.x = x * image.absolute_width
             image.y = y * image.absolute_height
-            print "%d,%d" % (image.x, image.y)
             x += 1
